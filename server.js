0ce1dabdd6cc5b16175b080f7eeec682730f7c49
Max McDonough <max.mcdonough@gmail.com>
2022-07-25 13:44:47 -0400

    Added Inquirer inputs

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 // Import Modules and files
-import express from 'express';
+// import express from 'express';
 import mysql from 'mysql2';
 import inquirer from 'inquirer';
+import ctable from 'console.table';
 // import getDepartments from './helper-files/queries.js';
 // import userInput from './helper-files/inquirer.js';
 // import questions from './helper-files/questions.js';
 
 
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
-// Set express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// // Set express middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // Create database connection
 const database = mysql.createConnection(
@@ -27,18 +28,32 @@ const database = mysql.createConnection(
 );
 
 // MySQL query to get departments
-function getDepartments() {
+function databaseQuery(sql) {
     return new Promise((resolve, reject) => {
-        database.query('SELECT name FROM department', (err, results) => {
+        database.query(sql, (err, results) => {
             err ? reject(err) : resolve(results);
         });
     });
 };
 
 // Async funtion to get departments
-async function setDepartments() {
-    let roleDepartment = await getDepartments();
-    console.log(roleDepartment);
+async function userInterface() {
+    // Pull choice lists from database for Inquirer prompt
+    let departmentList = await databaseQuery('SELECT name FROM department');
+    let roleList = await databaseQuery('SELECT title FROM role');
+    roleList = roleList.map(item => {return {name: item.title}});
+    let managerList = await databaseQuery('SELECT first_name, last_name FROM employee WHERE manager_id is NULL');
+    // managerFirstNames = managerList.map(item => item.first_name);
+    // managerLastNames = managerList.map(item => item.first_name);
+    // managerList = managerList.forEach(element => {
+        
+    managerList = managerList.map(item => {return {name: `${item.first_name} ${item.last_name}`}});
+    
+    // managerList = managerList.map(item => return {name: })
+    managerList.push({name: 'No Manager'});
+    console.log(departmentList);
+    console.log(roleList);
+    console.log(managerList);
     inquirer.prompt([
         {
             type: 'list',
@@ -68,8 +83,7 @@ async function setDepartments() {
             type: 'list',
             message: "Which Department is the new Role in?",
             name: 'newRoleDepartment',
-            // TO DO: Find way to input Department choices dynamically
-            choices: roleDepartment,
+            choices: departmentList,
             when: (answers) => answers.actionChoice === 'Add a Role',
         },
         {
@@ -88,8 +102,7 @@ async function setDepartments() {
             type: 'list',
             message: "What is the new Employee's Role?",
             name: 'newEmployeeRole',
-            // TO DO: Find way to input Role choices dynamically
-            choices: ["Marketing Manager", "Senior Developer", "Accountant"],
+            choices: roleList,
             when: (answers) => answers.actionChoice === 'Add an Employee',
         },
         {
@@ -97,7 +110,7 @@ async function setDepartments() {
             message: "Who is the new Employee's Manager?",
             name: 'newEmployeeManager',
             // TO DO: Find way to input Manager choices dynamically
-            choices: ["Robert Specktor", "Elizabeth Brokaw", "Patrick Whitacker", "Jessica Nysmith", "None"],
+            choices: managerList,
             when: (answers) => answers.actionChoice === 'Add an Employee',
         }
         // TO DO: Add prompts for Updating Employee Role
@@ -107,22 +120,17 @@ async function setDepartments() {
     });
 };
 
-// User Input function
-function userInput(questions) {
-    return inquirer.prompt(questions);
-};
-
 // Call database connection
 database.connect(function(err) {
     if (err) {
       return console.error('error: ' + err.message);
     };
-    setDepartments();
+    userInterface();
 });   
 
 // Set listening on PORT
-app.listen(PORT, () => 
-    console.log(`App listening on ${PORT}`)
-);
+// app.listen(PORT, () => 
+//     console.log(`App listening on ${PORT}`)
+// );
 
 // export default database;
